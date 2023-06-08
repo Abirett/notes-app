e079faa0a1ecdd8f85346fc56417f81cc31d5a4c
Ashfaque Hossain Abir <104485722+Abirett@users.noreply.github.com>
2023-06-08 12:11:08 +0600

    Disabled context menu & some keys

--- js/script.js
+++ js/script.js
@@ -107,3 +107,19 @@ addBtn.addEventListener("click", e => {
         closeIcon.click();
     }
 });
+
+
+//-------------- For prevent hacking -----------------
+
+// Disable context menu
+document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+   }, false);
+  
+   // Disable “Ctrl+Shift+I”, “Ctrl+U” and ”F12 key
+   document.addEventListener("keydown", (e) => {
+    if (e.ctrlKey || e.keyCode==123) {
+     e.stopPropagation();
+     e.preventDefault();
+    }
+   });
